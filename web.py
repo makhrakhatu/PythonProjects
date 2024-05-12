7da49f57cb47b4a8704ba50cf16c6e73d7575c6f
Lucky <makhrakhatu@hotmail.com>
2024-05-12 18:18:31 +0530

    Fifth commit to cherry-pick in develop branch

--- web.py
+++ web.py
@@ -2,3 +2,4 @@ import webbrowser
 webbrowser.open("https://www.youtube.com/"+"watch?v=CQBRCJem6BY")
 print("First line to cherry-pick : commit #2")
 print("Second line to cherry-pick : commit #3")
+print("Third line to cherry-pick : commit #5")
